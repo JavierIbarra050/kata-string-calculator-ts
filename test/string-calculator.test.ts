f12d3c875cab3a8561d4913a087cf3b367dce1a0
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-07-01 12:17:08 +0200

    [RED] - Create test empty string case

--- test/string-calculator.test.ts
+++ test/string-calculator.test.ts
@@ -1,5 +1,11 @@
 import { StringCalculator } from "../src/string-calculator";
 
 describe("StringCalculator", () => {
+    it("should return 0 when given an empty string", () => {
+        const calculator = new StringCalculator();
 
+        const result = calculator.add("");
+
+        expect(result).toBe(0);
+    });
 });
